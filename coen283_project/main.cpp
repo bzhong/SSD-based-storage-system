f460343951fd576b35f94511b3a954c053fa7bc1
Brady Zhong <bradyzhong2018@gmail.com>
2014-11-07 23:47:57 -0800

    create project structure

---
+++ coen283_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coen283_project
+//
+//  Created by Brady on 11/7/14.
+//  Copyright (c) 2014 Qianqian Zhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
